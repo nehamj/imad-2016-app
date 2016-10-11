14ddacf027694c165f49ea4cf236e96db539aa7b
nehamj <nehamj798@gmail.com>
2016-10-11 12:07:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,9 @@ var e=document.getElementById('mn');
 e.innerHTML="Web Application-1";
 
 var im=document.getElementById('img');
-marginRight=0;
+
 var moveright=function(){
-    marginRight=marginRight+1;
-    im.style.marginRight=marginRight+"px";
+   im.style.marginRight="1px";
 };
 im.onclick=function(){
   var interval=setInterval(moveright,20);

cb4d110ed8d17c99b2f7c1a2f8436677527f3454
nehamj <nehamj798@gmail.com>
2016-10-11 12:35:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,14 +5,13 @@ e.innerHTML="Web Application-1";
 var im=document.getElementById('img');
 var m=0;
 var p=0;
-var moveright=function(){
-   m=m+10;    
-   im.style.marginRight=m+"px";
-};
 im.onclick= function(){
-    p++;
-  var interval=setInterval(moveright);
-  if(p==6){
-      clearInterval(interval);
-  }
+    var interval=setInterval(function(){
+      p++;
+      m=m+10;    
+      im.style.marginRight=m+"px";
+      if(p==6){
+         clearInterval(interval);
+      }
+    });
 };

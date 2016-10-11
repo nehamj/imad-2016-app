fb8d33e28bd8a84dc450478f1fe6a6347e98bd17
nehamj <nehamj798@gmail.com>
2016-10-11 10:57:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,9 @@ app.get('/a2', function (req, res) {
   res.send(cc(second));
 });
 
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
